db34b25369f58d9481396419a2fc1b7c4cc0c05f
Tanmay-r <tvrandhavane@gmail.com>
2014-04-13 18:52:21 +0530

    Started coding the controller

---
+++ OpenProject/src/controller.h
@@ -0,0 +1,20 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class controller
+{
+    private:
+    	int m;
+
+    public:
+        controller();
+        void print(string s);
+};
+
+#endif // CONTROLLER_H
---
+++ OpenProject/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "controller.h"
+
+using namespace std;
+
+
+int main(int argc, char*argv[])
+{
+	controller * myController = new controller();
+
+	return 0;
+}
