3e196cdddc98c8903f2a34d1f135a3794dcd91c8
Tanmay-r <tvrandhavane@gmail.com>
2014-04-18 03:32:31 +0530

    pseudo code for foot placement control, pd controller

--- OpenProject/src/main.cpp
+++ OpenProject/src/main.cpp
@@ -1,38 +1,108 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 #include "controller.h"
 #include "state.h"
 
 using namespace std;
 
+enum leg_frame
+{
+	shoulders,
+	hip
+};
+
 state * state_parameters;
+void compute_tau_pd(vector<float> &tau);
+void compute_tau_vf(vector<float> &tau);
+void add_vector(vector<float> &result, vector<float> &opd1, vector<float> &opd2);
+void print_vector(vector<float> &vec);
+void get_tau_stance(leg_frame lf, bool l_stance, bool r_stance, vector<float> &vec);
+void compute_fv(float &fv);
 
-void compute_tau_pd(vector<float> &tau){
+/*
+void foot_placement_control(float swing_phase){
+	if(swing_phase == 0){
+		P1 = current location
+		P2 = Plf + (curr_velocity - target_velocity)*sfp;
+	}
 
+	position in ground plane = swing_phase*P1 + (1 - swing_phase)*P2
+	height = height_trajectory(swing_phase)
+	position = height + position in ground plane
+	return position
+}
+*/
+
+void compute_tau_pd(vector<float> &tau, float gait_phase){
+	//computation of target angle
+
+	//head
+		//state_parameters->get_head_position(gait_phase)
+		//current angles = state_parameters->get_current_angle()
+		//Apply Inverse Kinematics to get the angles of intermediate links
+		//Thus we get the target angles
+
+	//tail
+		//target angles are given as state parameters
+
+	//spine
+		//difference between quaternions
+
+	//legs
+		//for each leg in swing phase
+			//position = foot_placement_control(state_parameters->get_swing_phase(leg_id))
+			//then IK to get angles for intermediate links
+
+	/*
+	for each joint i
+		init_angle = current angle
+		target_angle = target angle
+		tau[i] = kp * (target_angle - init_angle) + kd * (target_angle - init_angle) / time_step
+	*/
 }
 
 void compute_tau_vf(vector<float> &tau){
-
+	float fv = 0;
+	compute_fv(fv);
 }
 
 void add_vector(vector<float> &result, vector<float> &opd1, vector<float> &opd2){
-	if((result.size != opd1.size) ||
-		(result.size != opd2.size) ||
-		(opd1.size != opd2.size)){
+	if((result.size() != opd1.size()) ||
+		(result.size() != opd2.size()) ||
+		(opd1.size() != opd2.size())){
 		cout << "ERROR: Vector sizes did not match in add vector" << endl;
 		exit(1);
 	}
-	for(int i = 0; i < result.size; i++){
+	for(int i = 0; i < result.size(); i++){
 		result[i] = opd1[i] + opd2[i];
 	}
 }
 
+void print_vector(vector<float> &vec){
+	for(int i = 0; i < vec.size(); i++){
+		cout << vec[i] << " ";
+	}
+	cout << endl;
+}
+
+void get_tau_stance(leg_frame lf, bool l_stance, bool r_stance, vector<float> &vec){
+
+}
+
+void compute_fv(vector<float> &fv){
+	for(int i = 0; i < fv.size(); i++){
+		fv[i] = kv*(state_parameters->vd[i] - state_parameters->v[i]);
+	}
+}
 
 int main(int argc, char*argv[])
 {
 	state_parameters = new state();
 
-	for(int i = 0; i < 1000; i++){
-		float gait_phase = state_parameters->get_gait_phase();
+	for(int i = 0; i < 2; i++){
+		float gait_phase = (float)state_parameters->get_gait_phase()/
+							(float)state_parameters->get_stride_period();
 
 		vector<float> tau_pd(state_parameters->get_number_joints());
 		vector<float> tau_vf(state_parameters->get_number_joints());
@@ -42,7 +112,17 @@ int main(int argc, char*argv[])
 		compute_tau_vf(tau_vf);
 
 		add_vector(tau_net, tau_pd, tau_vf);
+
+		//
+		cout << "tau_net = ";
+		print_vector(tau_net);
 		cout << "Time = " << i << endl;
+		//
+
+		get_tau_stance(shoulders, true, true, tau_net);
+		get_tau_stance(hip, true, true, tau_net);
+
+		state_parameters->set_gait_phase(state_parameters->get_gait_phase() + 1);
 	}
 
 	return 0;
